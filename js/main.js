385630b2ca06d3c88b028e1adf5e68cb80b19a46
subinaes34 <subinaes34@gmail.com>
2026-08-05 04:11:09 -0700

    added gallery page and slider light effect

--- js/main.js
+++ js/main.js
@@ -101,4 +101,7 @@ document.addEventListener('click',activate,false);
 
     
 })(jQuery);
-
+var quadimages = document.querySelectorAll("#quad figure");
+for(i=0; i<quadimages.length; i++) {
+  quadimages[i].addEventListener('click', function(){ this.classList.toggle("expanded"); quad.classList.toggle("full") }); 
+}
